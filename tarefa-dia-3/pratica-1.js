9854f5fd4e54100df0ec838a192885e366464c39
Your Name <you@example.com>
2022-05-19 16:02:16 -0300

    Mais valores

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -9,3 +9,9 @@ email = otaciliorezende85@gmail.com
 profissão = "estudante de programação"
 idade = 37
 cidade = "maragogi"
+
+console.log(nome)
+console.log(email)
+console.log(profissão)
+console.log = idade
+console.log(cidade)
