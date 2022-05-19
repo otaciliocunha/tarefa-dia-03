6728c5804862089b3308655542b354eaf76554b8
Your Name <you@example.com>
2022-05-19 16:17:18 -0300

    valores a viagem

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,8 +1,8 @@
-let nome
-let email
-let profissão
-let idade
-let cidade
+//let nome
+//let email
+//let profissão
+//let idade
+//let cidade
 
 nome = "Otacílio"
 email = otaciliorezende85@gmail.com
@@ -15,3 +15,10 @@ console.log(email)
 console.log(profissão)
 console.log = idade
 console.log(cidade)
+
+console.log(Olá, "nome!" Somos aqui da agência de Turismo
+Viagens dos Sonhos. Estamos escrevendo este email, pois 
+acabamos de confirmar as compras das passagens aéreas para "cidade".
+Confirma pra gente alguns dados? A sua idade é "idade", você é "profissão" email
+o seu email é "email"? Ficamos no aguardo. Muito obrigado
+e boa viagem)
